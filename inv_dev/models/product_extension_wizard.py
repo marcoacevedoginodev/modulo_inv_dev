3849ec7e2435308523d2f9e27a90b475499b1aac
Marco Acevedo <macevedo@fcg.cl>
2025-01-29 11:35:01 -0300

    aplicacion refactorizada y estable

--- inv_dev/models/product_extension_wizard.py
+++ inv_dev/models/product_extension_wizard.py
@@ -9,5 +9,23 @@ class ProductExtensionWizard(models.TransientModel):
     cantidad = fields.Integer(string='Cantidad')
 
     @api.model
-    def generate_zpl_label(self):
-        pass
+    def generate_zpl_label(self, *args, **kwargs):
+        zpl_labels = []
+        for record in self:
+            zpl = f"""
+            ^XA
+            ^FO50,50 
+            ^B3N,N,100,Y,N
+            ^FD>: {record.id_codigo}^FS
+            ^FO50,200
+            ^A0N,50,50
+            ^FDNumero: {record.id_numero}^FS
+            ^FO50,300
+            ^A0N,50,50
+            ^FDCantidad: {record.cantidad}^FS
+            ^FO50,400
+            ^GB800,3,3^FS            
+            ^XZ
+            """
+            zpl_labels.append(zpl)
+        return zpl_labels
